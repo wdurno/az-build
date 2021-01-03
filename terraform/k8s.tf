c0c4858b7ee0c657d835eb8d5eb35d065b57c713
wdurno <wdurno@gmail.com>
2021-01-03 01:17:16 +0000

    cost-effective changes

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "aks-cluster" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_B2S"
   }
 
   identity {
